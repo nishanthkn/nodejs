d88668491a6d2cc7e471bdfb40e32ab1b6b34377
nishanthkn <nishanthkn@outlook.com>
2024-03-12 12:04:44 +0530

    added work nishanth in the code

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World from Node.js!'));
+app.get('/', (req, res) => res.send('Hello World from nishanths Node.js!'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
