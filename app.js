6b2db40aca92bac7d0260efd5fb04328943b1c29
nishanthkn <nishanthkn@outlook.com>
2024-03-12 12:21:09 +0530

    added the word fun

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World from nishanths Node.js!'));
+app.get('/', (req, res) => res.send('Hello World from nishanths fun git push origin mainNode.js!'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
